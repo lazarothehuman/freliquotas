da206208015f13d9988ea39d525336c1ada4e2b6
lazarothehuman <lazaromathe@hotmail.com>
2018-08-09 11:42:32 +0200

    Usando sislog
    commit

--- freliquotas-base/src/mz/humansolutions/managers/NotificationManagerImp.java
+++ freliquotas-base/src/mz/humansolutions/managers/NotificationManagerImp.java
@@ -20,30 +20,22 @@ public class NotificationManagerImp implements NotificationManager {
 
 	@Override
 	public void sendSms(String recipient, String message) throws IOException {
-
-		try {
-			String username = "admin";
-			String password = "lj070797";
-			String originator = "06201234567";// dummy
-
-			String requestUrl = "http://127.0.0.1:9501/api?action=sendmessage&" + "username="
-					+ URLEncoder.encode(username, "UTF-8") + "&password=" + URLEncoder.encode(password, "UTF-8")
-					+ "&recipient=" + URLEncoder.encode(recipient, "UTF-8") + "&messagetype=SMS:TEXT" + "&messagedata="
-					+ URLEncoder.encode(message, "UTF-8") + "&originator=" + URLEncoder.encode(originator, "UTF-8")
-					+ "&serviceprovider=sislog" + "&responseformat=html";
-
-			URL url = new URL(requestUrl);
-			HttpURLConnection uc = (HttpURLConnection) url.openConnection();
-
-			System.out.println(uc.getResponseMessage());
-
+		String requestUrl = "http://67.43.3.105:9501/api?action=sendmessage&username=lazaroj&password=4K99Qj9e&recipient="
+				+ URLEncoder.encode(recipient, "UTF-8")
+				+ "&messagetype=SMS:TEXT&messagedata="
+				+ URLEncoder.encode(message, "UTF-8");
+		System.out.println(recipient + ", " + message);
+		URL url = new URL(requestUrl);
+		HttpURLConnection uc = (HttpURLConnection) url.openConnection();
+		try (BufferedReader bufferedReader = new BufferedReader(
+				new InputStreamReader(uc.getInputStream()))) {
+			String responseBody = bufferedReader.toString();
+			boolean sentSuccessfully = responseBody
+					.contains("<statuscode>0</statuscode>");
+			System.out.println("Enviado com sucesso: " + sentSuccessfully);
+		} finally {
 			uc.disconnect();
-
-		} catch (Exception ex) {
-			System.out.println(ex.getMessage());
-
 		}
-
 	}
 
 	@Override
@@ -59,7 +51,7 @@ public class NotificationManagerImp implements NotificationManager {
 				String message = "Caro(a) camarada, "+membro.getNome().toUpperCase()+" lembramos-lhe do seu dever de pagamento de QUOTAS e do envio do comprovativo a Sede da Cidade. Caso ja tenha feito, ignore esta mensagem.\n\nDAF Cidade de Maputo";
 				if (recipient != null) {
 					try {
-						sendSms(recipient, message);
+						sendSmsNotificationSislog(recipient, message);
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
