6ee58e554dbf36f373f7b35173b389d0aa78d77f
lazarothehuman <lazaromathe@hotmail.com>
2018-09-14 11:01:49 +0200

    Code changing

--- freliquotas-base/src/mz/humansolutions/utils/FrameManager.java
+++ freliquotas-base/src/mz/humansolutions/utils/FrameManager.java
@@ -280,7 +280,7 @@ public class FrameManager {
 	public void reportMembroMaps(User user) {
 		if (user != null) {
 			Profile profile = user.getProfile();
-			Transaccao transaction = dataManager.findTransaccao(401l);//still null
+			Transaccao transaction = dataManager.findTransaccao(501l);//still null
 			if (transaction.getProfiles().contains(profile))
 				load(transaction.getUrl());
 			else
